f4b79994633630b7f8844fbbd90b634efad82eb0
Nhilowtech <baonhi050502@gmail.com>
2023-05-09 21:38:56 +0700

    Update Project

---
+++ ConsoleApp1/programtaotoken.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace CreditCardApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=SQL8005.site4now.net;Initial Catalog=db_a98e60_bmtmdt;User Id=db_a98e60_bmtmdt_admin;Password=@Baonhi050502;Encrypt=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            Console.Write("Enter Credit Card Number: ");
+            string creditCardNumber = Console.ReadLine();
+
+            Console.Write("Enter CSV: ");
+            string csv = Console.ReadLine();
+
+            Console.Write("Enter Expiration Date (MM-YY): ");
+            string expirationDate = Console.ReadLine();
+
+            string idToken = "";
+            string csvToken = "";
+
+            // Generate unique IDToken and CSVToken
+            bool isIdTokenUnique = false;
+            bool isCsvTokenUnique = false;
+
+            while (!isIdTokenUnique || !isCsvTokenUnique)
+            {
+                idToken = GenerateRandomString(16);
+                csvToken = GenerateRandomString(12);
+
+                connection.Open();
+
+                // Check if IDToken already exists in database
+                SqlCommand idTokenCommand = new SqlCommand("SELECT COUNT(*) FROM CreditCardTable WHERE IDToken=@idToken", connection);
+                idTokenCommand.Parameters.AddWithValue("@idToken", idToken);
+
+                int idTokenCount = (int)idTokenCommand.ExecuteScalar();
+                if (idTokenCount == 0)
+                {
+                    isIdTokenUnique = true;
+                }
+
+                // Check if CSVToken already exists in database
+                SqlCommand csvTokenCommand = new SqlCommand("SELECT COUNT(*) FROM CreditCardTable WHERE CSVToken=@csvToken", connection);
+                csvTokenCommand.Parameters.AddWithValue("@csvToken", csvToken);
+
+                int csvTokenCount = (int)csvTokenCommand.ExecuteScalar();
+                if (csvTokenCount == 0)
+                {
+                    isCsvTokenUnique = true;
+                }
+
+                connection.Close();
+            }
+
+            // Insert new credit card record
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO CreditCardTable (CreditCardNumber, CSV, ExpirationDate, IDToken, CSVToken) VALUES (@creditCardNumber, @csv, @expirationDate, @idToken, @csvToken)", connection);
+            insertCommand.Parameters.AddWithValue("@creditCardNumber", creditCardNumber);
+            insertCommand.Parameters.AddWithValue("@csv", csv);
+            insertCommand.Parameters.AddWithValue("@expirationDate", expirationDate);
+            insertCommand.Parameters.AddWithValue("@idToken", idToken);
+            insertCommand.Parameters.AddWithValue("@csvToken", csvToken);
+
+            connection.Open();
+            insertCommand.ExecuteNonQuery();
+            connection.Close();
+
+            Console.WriteLine("Credit Card record has been added successfully!");
+        }
+
+        // Helper function to generate random string with specified length
+        static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ ConsoleApp3/programxacthuc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    class programxacthuc
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-3K158E8\\SQLEXPRESS;Database=token;Integrated Security = true";
+
+            Console.Write("Enter ID Token: ");
+            string idToken = Console.ReadLine();
+
+            Console.Write("Enter CSV Token: ");
+            string csvToken = Console.ReadLine();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("SELECT CreditCardNumber, ExpirationDate, CSV FROM CreditCardTable WHERE IDToken = @IDToken AND CSVToken = @CSVToken", connection);
+                    command.Parameters.AddWithValue("@IDToken", idToken);
+                    command.Parameters.AddWithValue("@CSVToken", csvToken);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string creditCardNumber = reader.GetString(0);
+                        string expirationDate = reader.GetString(1);
+                        string csv = reader.GetString(2);
+
+                        Console.WriteLine($"Credit Card Number: {creditCardNumber}");
+                        Console.WriteLine($"Expiration Date: {expirationDate}");
+                        Console.WriteLine($"CSV: {csv}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Token information is invalid.");
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
